8754a7275e64e22a75e2503567a4bd15705e81ee
Santiago Herrerea <santiherreravelas@gmail.com>
2023-09-06 15:19:18 -0500

    terrain

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = (transform.right * x) + (transform.forward * z);
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
